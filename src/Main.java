3e57d265ffe7782b87d1126e3b495f735c2da78b
Lucía Balsa <lbalsamartinez@danielcastelao.org>
2022-10-21 21:39:49 +0200

    Version 1.0 Ej1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // version 1.0
+        int base = 4, altura = 3, area;
+        area = base * altura / 2;
+        System.out.println("el area=" + area);
+    }
+}
